59ef2ee8e1b6887da346b3758a8659bb2316755e
stone <ClareZhou>
2018-12-21 17:16:09 +0800

    雏形

---
+++ PyMacroParser.py
@@ -0,0 +1,20 @@
+import math
+import sys
+import os
+
+class PyMacroParser(object):
+    def __init__(self):
+        pass
+    def load(self,f):   #其中f为文件路径，有两种异常跑出的方式
+        pass
+
+    def preDefine(self,s):
+        pass
+
+    def dumpDict(self):
+        pass
+        return dict
+
+    def dump(self,f):
+        pass
+
